9ac2a52d8b2840bee1588befb997902b256ef23d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2012-09-08 18:52:38 +0200

    Let's try with Firefox

--- testacular.conf.js
+++ testacular.conf.js
@@ -43,7 +43,7 @@ autoWatch = false;
 // - Opera
 // - Safari
 // - PhantomJS
-browsers = ['Chrome'];
+browsers = ['Firefox'];
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
